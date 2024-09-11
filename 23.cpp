5dd7b3b5913bcadc1177926744b46bf8ed5d30ce
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-11 22:35:20 +0530

    Add files via upload

---
+++ 23.cpp
@@ -0,0 +1,238 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+using namespace std;
+
+  
+//   int i = 1;
+
+//   while(i<=5){
+// cout<<i;
+// i+= 1;
+
+//   }
+
+// int n;
+// int sum = 0;
+// cin>>n;
+// while(n!=0){
+// int digit = n%10;
+// sum += digit;
+// n = n/10;
+// }
+// cout<<sum;
+// int arr[5];
+// for(int i = 0; i<5; i++){
+//     cin>>arr[i];
+// }
+// for(int i = 0; i<5; i++){
+//     cout<<arr[i];
+// }
+// 
+
+// vector<int>arr;
+// arr.push_back(9);
+// arr.push_back(7);
+// cout<<arr[1];
+
+//   pointer
+// int a = 10;
+// int *p = &a;
+// cout<<&a<<endl;;
+// cout<<p<<endl;;
+// cout<<*p;  dereference of the pointer and the reference means address
+
+
+
+// char a[] = {'o', ' ', 'l'};
+// cout<<a;
+// int count = 0;
+// int arr[] = {1,2,3,4,5};
+// for(int i = 0; i<5; i++){
+//     if(arr[i]%2 == 0){
+//         count ++;
+//     }
+
+// }
+// cout<<count;
+
+
+// OOPS
+// class Student{
+//     public:
+//     int rollno;
+//     void print(){
+//         cout<<"Roll Number = "<<rollno<<endl;
+//     }
+// };
+// int main(){
+//     Student s1;
+//     s1.rollno = 52;
+//     s1.print();
+//     Student s2;
+//     s2.rollno = 51;
+//     s2.print();
+// }
+
+
+// int maximum(int a, int b){
+//     if (a>b)
+//     return a;
+//     else
+//     return b;
+
+
+// }
+// int main(){
+// int c = maximum(4,5);
+// cout<<c;
+
+// }
+int main(){
+// int arr[7];
+// cout<<arr; 
+// char arr[106];
+// int arr[] = {2,4,6,9,12};
+
+// cout<<arr[0]<<endl;
+// for(int  i = 0; i<5; i++){
+//     cout<<arr[i]<<endl;
+// }
+
+// taking input from the user/
+
+// int arr[10];
+// for(int i = 0; i<10; i++){
+//     cin>>arr[i];
+// }
+// for(int i = 0; i<10; i++){
+// cout<<arr[i]<<endl;
+// }
+
+
+// int arr[5];
+// for(int i= 0; i<5; i++){
+//     cin>>arr[i];
+// }
+
+// for(int i = 0; i<5; i++){
+    
+//     cout<<(2*arr[i])<<endl;
+// }
+
+// int arr[] = {1,3,5,7,9};
+// for(int i = 0; i<5; i++){
+//     cout<<arr[i*0]<<endl;
+// }
+
+
+// counting number of zeros and ones in an array
+
+// int arr[] = {0,0,0,0,1,2,3,1,1,1,1,1,1,1,1,0,0,0};
+// int numZeros = 0;
+// int numOnes = 0;
+
+// for(int i = 0; i<18; i++){
+//     if (arr[i] == 0){
+//         numZeros++;
+//     }
+//     if(arr[i] == 1){
+//         numOnes++;
+//     }
+    
+//     cout<<numOnes<<endl;
+//     cout<<numZeros;
+
+   
+// }
+ 
+
+
+// maximum number present in  the array//
+
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//     int size = 10;
+//     int max = INT_MIN;
+//     for(int i = 0; i<size; i++){
+//         if(arr[i]> max){
+//             max = arr[i];
+//         }
+//     }
+
+// cout<<max;
+
+
+// Extreme  
+
+// int arr[8] = {10,20,30,40,50,60,70,80};
+// int size = 8;
+// int start = 0;
+// int end = size-1;
+// while(true){
+// if(start > end){
+//     break;
+// }
+//     cout<<arr[start]<<endl;
+//     cout<<arr[end]<<endl;
+//     start++;
+//     end--;
+
+
+
+
+
+
+ 
+// }
+
+
+// int arr[] = {10,20,30,40,50,60,70,80};
+// int size = 8;
+// int start = 0;
+// int end = size-1;
+
+
+// while(start<=end){
+//     swap(arr[start], arr[end]);
+
+//     start++;
+//     end--;
+// }
+// for(int i = 0; i<size; i++){
+//     cout<<arr[i]<<" ";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
