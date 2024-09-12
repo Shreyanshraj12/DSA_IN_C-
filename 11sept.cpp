3920e51f0fa54a954033d6b7ad2239428e2ff53a
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-12 11:36:22 +0530

    Add files via upload
    TWO DIMENSIONAL ARRAYS PROBLEMS

---
+++ 11sept.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// int Max(int arr[3][3],int row, int col){
+//       int max = INT_MIN;
+//     for(int i = 0; i<row; i++){
+      
+//         for(int j = 0; j<col; j++){
+//             if(arr[i][j] > max){
+//                 max = arr[i][j];
+//             }
+//         }
+//     }
+//     return max;
+// }
+
+// void RowSum(int arr[3][3],int row, int col){
+//     for(int i = 0; i<row; i++){
+//         int sum = 0;
+//         for(int j = 0; j<col; j++){
+//             sum  = sum+arr[i][j];
+//         }
+//         cout<<sum<<" ";
+       
+//     }
+
+// }
+
+
+// bool Search(int arr[3][3], int row,int col,int key){
+//     for(int i = 0; i<row; i++){
+//         for(int j = 0; j<col; j++){
+//             if(arr[i][j] == key){
+//             return true;
+//             }
+//         }
+       
+       
+//     }
+//      return false;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main(){
+
+// // ROW WISE SUM
+// // col wise sum
+// int row = 3;
+// int col = 3;
+// int arr[3][3];
+
+// for(int i = 0; i<row; i++){
+//     for(int j = 0; j<col; j++){
+//         cin>>arr[i][j];
+//     }
+// }
+
+
+// cout<<endl;
+
+// for(int i = 0; i<row; i++){
+//     for(int j = 0; j<col; j++){
+//         cout<<arr[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// cout<<endl;
+
+// RowSum(arr,row,col);
+// int Linear_Search = Search(arr,row,col,5);
+// cout<<Linear_Search;
+
+
+// cout<<Max(arr,row,col);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
