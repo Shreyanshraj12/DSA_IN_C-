4f3b7545f4efc264db967a6fe9008cd397dca41d
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-12 18:51:18 +0530

    Add files via upload

---
+++ problems_on_binarySearch.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int lastoccerence(vector<int> v,int target){
+//     int start = 0;
+//     int end = v.size()-1;
+//     int mid;
+//     int store = -1;
+// while(start<=end){
+//     mid = (start+end)/2;
+// if(v[mid] == target){
+// store = mid;
+// start = mid+1;
+// }
+
+// else if(v[mid]<target){
+//     start = mid+1;
+// }
+
+// else if(v[mid]>target){
+//     end = mid-1;
+// }
+// }
+
+// return store;
+
+
+
+
+
+// }
+
+
+
+
+
+
+int main(){
+
+
+
+// vector<int>v{1,3,7,7,7,7,7,7,7,7,7,7,7,7,7,7,9};
+// int target = 7;
+// int ans = lastoccerence(v,target);
+// cout<<ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
