a3c6be5b79a8dbebba4954308b49b7c323c8a657
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-13 22:33:03 +0530

    Add files via upload

---
+++ _char.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// int getlength(char name[]){
+//     int length = 0;
+//     int i = 0;
+//     while(name[i] != '\0'){
+//         length++;
+//         i++;
+//     }
+//     return length;
+// }
+
+
+// void RemoveSpace(char arr[]){
+//     // int i = 0;
+//     int n = strlen(arr);
+
+//     for(int i = 0; i<n; i++){
+// if(arr[i] == ' '){
+//     arr[i] = '@';
+// }
+//     }
+// }
+
+
+// bool Palindrome(char arr[]){
+//     int i = 0;
+//     int j = strlen(arr)-1;
+//     while(i<=j){
+//         if(arr[i] == arr[j]){
+//             i++;
+//             j--;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+// void Uppercase(char arr[]){
+// int n = strlen(arr);
+// for(int i = 0; i<n; i++){
+//     // if(arr[i]>= 'a' && arr[i]<='z')   example Shreyansh =>SHREYANSH
+// arr[i] = (arr[i]-'a'+'A');
+// // arr[i] = (arr[i]-32);  ek hi baat hai. time complexity O(n)
+// }
+
+// }
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+    // length of the string
+
+// char name[100];
+// cin>>name;
+// int ans = getlength(name);
+// cout<<ans<<endl;
+// cout<<strlen(name); 
+
+
+//  Question Replace All Space.
+
+
+// char arr[100];
+// cin.getline(arr,50);
+// RemoveSpace(arr);
+// cout<<arr;
+
+
+
+
+// Palindrome Question
+
+// char arr[100];
+// cin>>arr;
+// int ans = Palindrome(arr);
+// cout<<ans;
+
+
+
+
+
+// char arr[100];
+// // cin.getline(arr,50);
+// cin>>arr;
+// Uppercase(arr);
+// cout<<arr<<endl;
+
+
+
+
+
+
+
+
+ 
+
+
+}
