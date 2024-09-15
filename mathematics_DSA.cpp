d99536bb75e5f5a671bb3ab7dcbc6d7e643733f1
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-15 21:38:05 +0530

    Add files via upload

---
+++ mathematics_DSA.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int Expo(int a, int b){
+//     int ans = 1;
+//     for(int i = 0; i<b; i++){
+//         ans = ans*a;
+//     }
+//     return ans;
+// }
+
+
+
+
+
+// int main(){
+
+// int a = 2;
+// int b = 2;
+// int ans = Expo(a,b);
+// cout<<ans;
+
+// }
+
+// TIME COMPLEXITY O(B).
+
+
+
+
+// Fast Exponention
+// int FastExpo(int a,int b){
+//     int ans = 1;
+
+//     while(b>0){
+//         if(b & 1){
+//             ans = ans*a;
+//         }
+//         a = a*a;
+//         b>>=1;  b = b/2
+//     }
+//     return ans;
+// }
+
+// time complexity o(log b)
+
+int main(){
+    // int a = 2;
+    // int b = 10;
+    // int result = FastExpo(a,b);
+    // cout<<result;
+
+
+
+
+}
