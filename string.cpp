764b7548d5b5846e306dd4fae1cce7245f55c64f
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-15 13:30:12 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,100 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+
+// bool Compare(string a,string b){
+//     if(a.length() != b.length()){
+//         return false;
+//     }
+//     else{
+//         int j = 0;
+//         for(int i = 0; i<a.length(); i++){
+//             // int j = 0;
+//             if(a[i] == b[j]){
+//                 return true;
+//             }
+//             j++;
+//         }
+//     }
+//     return false;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// String => Sequence of Character 
+// string str;
+
+
+// cin>>str;
+// getline(cin,str);
+// cout<<str;
+
+// string a;
+// string b;
+// cin>>a;
+// cin>>b;
+// int ans = Compare(a,b);
+// cout<<ans;
+
+
+// string sentence = "Hello Jee Kaise Ho Saare";
+// string target = "Hello";
+// cout<<sentence.find(target);
+
+
+
+// bool cmp(char first,char second){
+//     return first>second;
+// }
+
+// int cmp(int x,int y){
+//     return x>y;
+// }
+
+
+// int main(){
+
+
+
+// string s = "babbar";   //a<b<c<d<e< lexicographical order.
+// sort(s.begin(), s.end(),cmp);
+
+
+// vector<int>v{5,3,1,2,4};
+// sort(v.begin(),v.end(),cmp);
+// for(int i = 0; i<v.size();i++){
+//     cout<<v[i]<<endl;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cout<<s;
+
+
+
+
+// }
+
+
+
+
