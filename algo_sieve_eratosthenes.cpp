d5742579260bc87c241632f9b4d515be83faa121
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-15 17:45:19 +0530

    Add files via upload

---
+++ algo_sieve_eratosthenes.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// bool IsPrime(int n){
+//     if(n<=1){
+//         return false;
+//     }
+//     for(int i = 2; i<n; i++){
+//         if(n%i == 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+
+
+// int main(){
+// int n;
+// cin>>n;
+// int count = 0;
+// for(int i = 0; i<n; i++){
+//    if( IsPrime(i)){
+//     ++count;
+//    }
+// }
+// cout<<count<<endl;
+// }
+
+// ALGORITHM OF SIEVE OF ERATOSTHENES. TIME COMPLEXITY O(N LOGLOGN)
+
+// int countPrimes(int n){
+//     if (n == 0){
+//         return false;
+//     }
+
+
+
+    
+// vector<bool> prime(n,true);
+// prime[0] = prime[1] = false;
+// int ans = 0;
+
+// for(int i = 2; i<n; i++){
+//     if(prime[i]){
+//         ans++;
+
+//         int j = 2*i;
+//         while(j<n){
+//             prime[j] = false;
+//             j = j+i;
+//         }
+//     }
+// }
+
+
+
+// }
+
+
+
+
