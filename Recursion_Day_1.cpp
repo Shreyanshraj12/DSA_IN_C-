a35429919143bf528d25bf11ef2095b40a023726
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-16 18:41:11 +0530

    Add files via upload

---
+++ Recursion_Day_1.cpp
@@ -0,0 +1,142 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// HAID RECURSION AND TAIL RECURSION ARE THE TYPES OF RECURSION.
+
+
+// void count(int n){
+// if (n == 0){
+//     return;
+// }
+
+// int digits = n%10;
+// count(n/10);
+// cout<<digits<<" ";
+
+
+// }
+
+
+
+
+
+
+// void print(int arr[],int size,int i){
+//     while(i>=size){
+//         return;
+//     }
+
+//     // cout<<arr[i]<<" ";
+
+//     print(arr,size,i+1);
+    
+  
+// }
+
+
+// Fibonacci Series =====> 0,1,1,2,3,5,8,13.
+
+// int fib(int n){
+// first term
+// if(n == 1){
+//     return 0;
+// }
+// Second term
+// if(n == 2){
+//     return 1;
+// }
+//  int ans = fib(n-1)+fib(n-2);
+//  return ans;
+
+
+// }
+
+
+
+// Counting.
+// void count(int n){
+//     if(n == 0){
+//         return;
+//     }
+//     cout<<n<<endl;
+//     count(n-1);
+//     // cout<<n<<endl;
+// }
+
+
+
+
+
+// int Fact(int n){
+//     // base condition
+//     if(n==1 || n == 0){
+//         return 1;
+// }
+// return n*Fact(n-1);
+// }
+
+// int fact(int n){
+//     if(n ==1 || n == 0){
+//         return 1;
+
+//     }
+//     return n*fact(n-1);
+// }
+
+
+// int main(){
+//     // int a = 5;
+//     int ans = fact(0);
+//     cout<<ans;
+// }
+
+int main(){
+
+    // int n;
+    // cin>>n;
+    // int ans = Fact(n);
+    // cout<<ans;
+
+
+    // int n;
+    // cin>>n;
+
+// count(5);
+
+
+
+// int n;
+// cout<<"enter the term you want to see"<<endl;
+// cin>>n;
+// int ans = fib(n);
+// cout<<ans;
+
+// int arr[5] = {10,20,30,40,50};
+// int size = 5;
+// int i = 0;
+// print(arr,size,i);
+
+
+
+// int a = 647;
+
+// count(a);
+
+
+// int n = 0647;
+// cout<<n;
+
+
+ 
+
+
+}
+
+
+
+
+
+
+
+
+
