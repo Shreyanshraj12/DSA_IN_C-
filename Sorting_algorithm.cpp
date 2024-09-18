9bc33ddfa4d42761ac6bc70d46f68f23802790c5
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-19 00:28:09 +0530

    Add files via upload

---
+++ Sorting_algorithm.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Sorting
+// BUBBLE SORT ALGORITHM IS STABLE ALGORITHM, COST OF SWAPPING IS HIGH
+int main(){
+
+// int arr[] = {5,4,3,2,1};
+// int n = 5;
+// for(int i = 0; i <n-1; i++){
+//     for(int j = 0; j<n-1-i; j++){
+//         if(arr[j] > arr[j+1]){
+//             swap(arr[j], arr[j+1]);
+//         }
+//     }
+// }
+
+// for(int i = 0; i<n; i++){
+//     cout<<arr[i]<<" ";
+// }
+
+
+
+// SELECTION SORT ALGORITHM IS AN UNSTABLE ALGORITHM
+// COST OF SWAPPING LESS.
+
+int arr[] = {5,3,1,4,2};
+int n = 5;
+
+
+
+// for(int i = 0; i<n-1; i++){
+//     int min = INT_MAX;
+//     int minindex = -1;
+//     for(int j = i; j<n; j++){
+//         if(arr[j]<min){
+//             min = arr[j];
+//  minindex = j;
+//        }
+//     }
+//     swap(arr[i],arr[minindex]);
+// }
+
+// for(int i = 0; i<n; i++){
+//     cout<<arr[i]<<" ";
+// }
+
+
+
+// INSERTION SORT ALGORITHM
+
+
+// for(int i = 1; i<n; i++){
+//     int j = i;
+// while(j>=1){
+//     if(arr[j]>=arr[j-1]){
+//         break;
+//     }
+//     else{
+//         swap(arr[j],arr[j-1]);
+//         j--;
+//     }
+// }
+// }
+
+
+// for(int i = 0; i<n; i++){
+//     cout<<arr[i]<<" ";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
