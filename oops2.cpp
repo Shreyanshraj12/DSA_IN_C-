cdf022e4451b390ca3e94b321d6aa43e16fcc0f6
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-22 21:37:03 +0530

    Add files via upload

---
+++ oops2.cpp
@@ -0,0 +1,178 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// class Animal{
+// public:
+//     int age;
+//     int weight;
+
+//     void eat(){
+//         cout<<"Eating"<<endl;
+//     } 
+
+
+// };
+
+
+// class Dog:public Animal{
+
+
+// };
+
+
+// class Car{
+//     public:
+// string name;
+// int weight;
+// int age;
+
+// void speedUp(){
+//     cout<<"Speeding Up"<<endl;
+// }
+
+// void breakMarro(){
+//     cout<<"Break Maardi"<<endl;
+// }
+
+
+// };
+
+// class Scorpio:public Car{
+
+// };
+
+
+// class Fruit{
+//    public:
+//    string name;
+// };
+
+// class Mango: public Fruit{
+// public:
+// int weight;
+// };
+
+
+// class Alphanso: public Mango{
+// public:
+// int sugarLevel;
+// };
+
+// class A{
+//     public:
+//     int chemistry;
+
+//     A(){
+//         chemistry = 101;
+//     }
+
+// };
+
+// class B{
+//     public:
+//     int chemistry;
+
+//     B()
+//     {
+//         chemistry = 1234;
+//     }
+
+// };
+
+
+// class C: public A, public B{
+//     public:
+//     int maths;
+    
+    
+//     };
+
+// class Car{
+//     public:
+//     int age;
+//     int weight;
+//     string name;
+//     void SpeedUp(){
+//         cout<<"SpeedingUp"<<endl;
+//     }
+
+// };
+
+// class Scorpio: public Car{
+
+// };
+
+// class Fortuner:public Car{
+ 
+// };
+
+// OPERATOR OVERLOADING
+// class Maths{
+// public:
+// int sum(int a, int b){
+//     cout<<"I am in the first Signature"<<endl;
+//     return a+b;
+// }
+
+
+// int sum(int a, int b, int c){
+//     return a+b+c;
+// }
+
+// int sum(int a, float b){
+//     return a+b;
+// }
+
+// };
+
+
+
+int main(){
+
+    // Dog d1;
+    // d1.age;
+    // d1.eat();
+
+// Scorpio S;
+// S.speedUp();
+
+// Alphanso a;
+// cout<<a.name << " " << a.weight << " " <<a.sugarLevel;
+
+// C obj;
+// cout<<obj.B::chemistry<<" "<<obj.maths<<endl;
+
+// Scorpio s11;
+// s11.SpeedUp();
+
+// Fortuner f11;
+// f11.SpeedUp();
+// Maths obj;
+// cout<<obj.sum(2,5,10)<<endl;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
