b8ef4392272317e9bb4e4414784d313a6673f90e
Shreyansh Raj <111487233+Shreyanshraj12@users.noreply.github.com>
2024-09-22 16:44:37 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,133 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// class Animal{
+// // int age;
+// // int weight;
+// // char ch;
+// private:
+// // int weight;
+// public:
+// int weight;
+// int age;
+// string name;
+// // Default Constructor
+// Animal(){
+//     this->weight = 0;
+//     this->age = age;
+//     this->name = "";
+//     cout<<"Constructor called"<<endl;
+// }
+
+// // Parameterised Constructor
+// Animal(int age){
+//     this->age = age;
+//     cout<<"Parameterised Constructor Called"<<endl;
+// }
+
+// Animal(int age, int weight){
+//     this->age = age;
+//     this->weight = weight;
+//     cout<<"Parameterised constructor 2 is called"<<endl;
+// }
+// Animal(int age,int weight, string name){
+//     this->age = age;
+//     this->weight = weight;
+//     this->name = name;
+//     cout<<"parameterised constructor 3 is called"<<endl;
+// }
+
+// // Copy Constructor
+// Animal(Animal &obj){
+//     this->age = obj.age;
+//     this->weight = obj.weight;
+//     this->name = obj.name;
+//     cout<<"Copy Constructor Called"<<endl;
+// }
+
+
+
+
+
+// void eat(){
+// cout<<"Eating"<<endl;
+// }
+
+// void sleep(){
+// cout<<"Slepping"<<endl;
+// }
+
+// int getWeight(){
+//     return weight;
+// }
+// void SetWeight(int weight){
+// this->weight = weight;
+// }
+
+// // DESTRUCTOR
+// ~Animal(){
+// cout<<"I am inside destructor"<<endl;
+// }
+
+
+// };
+
+
+
+
+
+
+
+int main(){
+// object creation
+// static
+// Animal ramesh;
+// ramesh.age = 12;
+// ramesh.name = "Lion";
+// cout<<"age of ramesh is" <<ramesh.age<<endl;
+// cout<<"name of ramesh is "<<ramesh.name<<endl;
+// ramesh.eat();
+// ramesh.sleep();
+ 
+// ramesh.SetWeight(101);
+// ramesh.getWeight();
+// cout<<"Weight "<< ramesh.getWeight()<<endl;
+
+// cout<<sizeof(Animal);
+// Dynamic Memory Allocation
+// Animal *suresh = new Animal;
+// (*suresh).age  = 15;
+// cout<<(*suresh).age<<endl;
+// suresh->age = 15;
+// suresh->name = "dog";
+// suresh->eat();
+// suresh->sleep();
+
+// Animal a(21,100);
+// cout<<a.age<<endl;
+// cout<<a.weight<<endl;
+
+// Animal a(21,200,"Suresh");
+// Animal *a = new Animal(21,100,"sheru");
+
+// cout<<a->age<<endl;
+// cout<<a->weight<<endl;
+// cout<<a->name<<endl;
+
+
+
+// Animal *b = new Animal(100,10,"Babbar");
+// copy constructor
+// Animal a;
+// Animal c = a;
+
+// Animal a;
+// a.age = 5;
+
+// Animal *b = new Animal();
+// b->age = 12;
+
+
+// maually
+// delete b;
+}
